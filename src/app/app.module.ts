b2e21909c17c03a49ad9b3579e7462e7d088b10e
javier castro <javiercastro27@outlook.es>
2023-04-09 23:08:40 -0500

    🔃 update libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { NavComponent } from './nav/nav.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MenubarModule,
+    InputTextModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,38 @@
+// nav.component.ts
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+  items: MenuItem[] = [];
+  search: string = '';
 
+  constructor() {
+    this.items = [
+      {
+        label: 'Inicio',
+        icon: 'pi pi-home',
+        routerLink: '/'
+      },
+      {
+        label: 'Productos',
+        icon: 'pi pi-fw pi-shopping-cart',
+        routerLink: '/productos'
+      },
+      {
+        label: 'Contacto',
+        icon: 'pi pi-fw pi-envelope',
+        routerLink: '/contacto'
+      },
+      {
+        label: 'Acerca de',
+        icon: 'pi pi-fw pi-info-circle',
+        routerLink: '/acerca-de' // Agregar la propiedad 'routerLink' aquí
+      }
+    ];
+  }
 }
